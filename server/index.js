a9a940e742a110a8054feea422ee6d3521c819b9
Rimvydas Kulda <rimvydas.kulda@gmail.com>
2017-09-16 12:51:17 +0300

    added firebase connection

--- server/index.js
+++ server/index.js
@@ -1,16 +1,40 @@
 const express = require('express');
 const path = require('path');
+var admin = require('firebase-admin');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+var serviceAccount = require("./key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://quiz-homework.firebaseio.com"
+});
+
+var db = admin.database();
+var ref = db.ref("server/saving-data/fireblog");
+
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+
+ref.on("value", function(snapshot) {
+  console.log(snapshot.val());
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
 
 // Answer API requests.
 app.get('/api', function (req, res) {
   res.set('Content-Type', 'application/json');
   res.send('{"message":"Hello from the custom server!"}');
+  var usersRef = ref.child("users");
+  usersRef.set({
+    rimvydas_kulda: {
+      date_of_birth: "June 23, 1912",
+      full_name: "Alan Turing"
+    }
+  });
 });
 
 // All remaining requests return the React app, so it can handle routing.
