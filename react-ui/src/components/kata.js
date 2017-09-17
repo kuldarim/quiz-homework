3a1258d327b8f35862de3e7a84690867d6725317
Rimvydas Kulda <rimvydas.kulda@gmail.com>
2017-09-17 20:12:53 +0300

    added editor

--- react-ui/src/components/kata.js
+++ react-ui/src/components/kata.js
@@ -1,29 +1,14 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-
-import AceEditor from 'react-ace';
-import 'brace/mode/javascript';
-import 'brace/theme/monokai';
-
-import { putChangeTestStatus, putChangeSolution } from '../redux/reducers/reducer';
 
 import MarkDown from './markdown';
-import { Button } from 'reactstrap';
 import RunTests from './run-tests';
+import Editor from './editor';
 import './kata.css';
 
 class Kata extends Component {
   
-  onChange(newValue) {
-    this.props.putChangeSolution(this.props.kata.id, newValue)
-  }
-  
   constructor(props) {
     super(props);
-    this.state = {
-      solution: () => {}
-    };
-    this.onChange = this.onChange.bind(this);
   }
   
   render() {
@@ -31,23 +16,12 @@ class Kata extends Component {
       <div>
         <MarkDown kata={this.props.kata} />
         <div className="editor-container">
-          <AceEditor
-            mode="javascript"
-            theme="monokai"
-            name={`${this.props.kata.id}`}
-            onChange={this.onChange}
-            value={this.props.kata.solution}
-            setOptions={{
-              showLineNumbers: true,
-              tabSize: 2,
-            }}
-          />
-          <RunTests kata={this.props.kata}/>
+          <Editor kata={this.props.kata} />
+          <RunTests kata={this.props.kata} />
         </div>
       </div>
     );
   }
 }
 
-const mapDispatch = {putChangeTestStatus, putChangeSolution};
-export default connect(null, mapDispatch)(Kata);
+export default Kata;
