15d4a4e83136c4f028999040026b8f4074867c37
Rimvydas Kulda <rimvydas.kulda@gmail.com>
2017-09-17 21:30:51 +0300

    added alert

--- react-ui/src/components/submit.js
+++ react-ui/src/components/submit.js
@@ -2,19 +2,42 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { Button } from 'reactstrap';
 import { putSubmit } from '../redux/reducers/reducer';
+import Alerts from './alerts';
 
 class Submit extends Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      missing: {}
+    }
+  }
+
+  submit() {
+    const withoutSolution = this.props.katas.filter(({solution}) => !solution);
+    if (this.props.user && !withoutSolution.length) {
+      this.setState({missing: {submit: true}});
+      this.props.putSubmit(this.props.katas, this.props.user)
+    } else {
+      
+      this.setState({
+        missing: {
+          user: !this.props.user,
+          katas: !!withoutSolution.length
+        }
+      })
+    }
   }
 
   render() {
     return (
       this.props.index === this.props.katas.length - 1
-        ? <Button 
-            onClick={() => this.props.putSubmit(this.props.katas, this.props.user)} 
-            color="primary"
-          >Submit</Button> 
+        ? <div>
+            <Button 
+              onClick={() => this.submit()} 
+              color="primary"
+            >Submit</Button>
+            <Alerts missing={this.state.missing}/>
+          </div> 
         : null       
     );
   }
