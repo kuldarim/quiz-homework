c4dcf920b7aeaab8c2ddae377cb6bb253c15e3de
Rimvydas Kulda <rimvydas.kulda@gmail.com>
2017-09-20 21:05:01 +0300

    removed logger for prod build

--- react-ui/src/redux/store.ts
+++ react-ui/src/redux/store.ts
@@ -1,13 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducers/reducer';
 import thunk from 'redux-thunk';
-const logger = require('redux-logger').default;
+// const logger = require('redux-logger').default;
 
 const store = createStore(
   reducer,
   applyMiddleware(
     thunk,
-    logger,
+    // logger,
   ),
 );
 
